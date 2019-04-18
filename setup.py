de65824780a7e35693ab8ef66ec268ec421fe736
romanstingler <coolx67@gmx.at>
2019-04-18 15:59:20 +0200

    Added License and initial setup files

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+setup(
+ 
+    name = "Corsair Link",
+    version = "0.1",
+    packages = find_packages(),
+ 
+    entry_points = {
+        'console_scripts': [
+            'cl = cl:main'
+        ]
+    },
+ 
+    # metadata for upload to PyPI
+    author = "Roman Stingler",
+    author_email = "roman.stingler@symvaro.com",
+    description = "A tool to communicate with Corsair link",
+ 
+    license = "GPL2",
+    keywords = "Corsair link linux",
+    url = "https://github.com/",
+ 
+    install_requires=[
+        'ioctl-opt>=1.2'
+    ],
+ 
+)
